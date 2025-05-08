95a6e36cb51389e7412eb9c9b7241766de6a23bd
Gabrielė Drungilaitė <120126780+Ulomei@users.noreply.github.com>
2025-05-09 01:58:30 +0300

    Menu has a nice layout and is working with database. Static image of product is not loading.

--- src/main/java/org/coffee/web/ShowPorductBean.java
+++ src/main/java/org/coffee/web/ShowPorductBean.java
@@ -10,13 +10,34 @@ import org.coffee.util.PasswordUtil;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named
-@RequestScoped
-public class ShowPorductBean {
+@ViewScoped
+@Getter
+@Setter
+public class ShowPorductBean implements Serializable {
+
     @Inject
     private ProductDAO productDAO;
+
+    private List<Product> productList;
+
+    public List<Product> getProductList() {
+        if (productList == null) {
+            productList = productDAO.findAll();
+        }
+        return productList;
+    }
+
+    public void refreshProducts() {
+        productList = productDAO.findAll();
+    }
+
 }
