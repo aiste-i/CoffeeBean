363da721fefcae131b375478e871e04faf776a92
Gabrielė Drungilaitė <120126780+Ulomei@users.noreply.github.com>
2025-05-27 12:19:52 +0300

    Change from long to Long in ingredient.java

--- src/main/java/org/coffee/persistence/entity/Ingredient.java
+++ src/main/java/org/coffee/persistence/entity/Ingredient.java
@@ -19,7 +19,7 @@ public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "name", unique = true, nullable = false)
     private String name;
